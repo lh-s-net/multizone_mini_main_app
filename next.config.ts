393b2f58943fc8458e731f20434189b09295e3af
JW <info@lh-s.net>
2025-07-15 23:08:38 +0200

    Add Content-Security-Policy headers and enable React strict mode in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,43 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
+  reactStrictMode: true,
+  async headers() {
     return [
-      // Rewrites für sup-app
       {
-        source: '/sub-app',
-        destination: `${process.env.NEXT_PUBLIC_URL_SUP_APP}/sub-app`
-      },
-      {
-        source: '/sub-app/:path*',
-        destination: `${process.env.NEXT_PUBLIC_URL_SUP_APP}/sub-app/:path*`
+        // Set headers for all routes
+        source: "/(.*)", // This applies to all routes in the application
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'none';",
+              "script-src 'strict-dynamic' 'self' https://cdn.jsdelivr.net;",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
+              "font-src 'self' https://fonts.gstatic.com;",
+              "connect-src 'self' https://*.clerk.dev https://tschug.com;",
+              "img-src 'self' data: blob: https://tschug.com;",
+              "base-uri 'self';",
+              "form-action 'self';",
+              "frame-ancestors 'self';",
+            ].join(" "),
+          },
+        ],
       },
     ];
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/sub-app",
+        destination: `${process.env.NEXT_PUBLIC_URL_SUP_APP}/sub-app`,
+      },
+      {
+        source: "/sub-app/:path*",
+        destination: `${process.env.NEXT_PUBLIC_URL_SUP_APP}/sub-app/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
