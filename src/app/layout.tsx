e3434197cd3425bd9bd01681603ca5dd17fea448
JW <info@lh-s.net>
2025-07-27 12:29:59 +0200

    Remove unused Clerk authentication components and headers import from layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
-import {ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
-import { headers } from "next/headers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,15 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     // @ts-ignore
-      <html lang="en">
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <header className="flex justify-end items-center p-4 gap-4 h-16">
-      </header>
-      {children}
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+        </header>
+        {children}
       </body>
-      </html>
+    </html>
   )
     ;
 }
